416c47e7d75c0780825dcfc07cf23512718f4ec2
Ateeksh <ateesood@gmail.com>
2026-01-04 17:06:44 +0000

    debugging and testing done

--- week5/app.py
+++ week5/app.py
@@ -8,21 +8,21 @@ db.init_app(app)
 
 class Student(db.Model):
     __tablename__ = 'student'
-    student_id = db.Column(db.Integer , primary_key = True , auto_increment=True)
+    student_id = db.Column(db.Integer , primary_key = True , autoincrement=True)
     roll_number = db.Column(db.String , unique=True , nullable = False)
     first_name = db.Column(db.String , nullable = False)
     last_name = db.Column(db.String)
     
 class Course(db.Model):
     __tablename__ = 'course'
-    course_id = db.Column(db.Integer , primary_key=True , auto_increment=True)
+    course_id = db.Column(db.Integer , primary_key=True , autoincrement=True)
     course_code = db.Column(db.String , unique=True , nullable = False)
     course_name = db.Column(db.String ,  nullable = False)
     course_description = db.Column(db.String)
     
 class Enrollments(db.Model):
     __tablename__ = 'enrollments'
-    enrollment_id = db.Column(db.Integer , primary_key = True , auto_increment=True)
+    enrollment_id = db.Column(db.Integer , primary_key = True , autoincrement=True)
     estudent_id = db.Column(db.Integer , db.ForeignKey("student.student_id") , nullable=False)
     ecourse_id = db.Column(db.Integer , db.ForeignKey("course.course_id") , nullable=False)
     
@@ -53,8 +53,8 @@ def create_student():
             db.session.add(new_student)
             db.session.commit()
             added_student = Student.query.filter_by(roll_number = roll_number).first()
-            for course_name in courses:
-                course = Course.query.filter_by(course_name = course_name).first()
+            for course_code in courses:
+                course = Course.query.filter_by(course_code = course_code).first()
                 enrollment = Enrollments(estudent_id=added_student.student_id , ecourse_id = course.course_id)
                 db.session.add(enrollment)
             db.session.commit()
@@ -66,10 +66,10 @@ def create_student():
 @app.route('/student/<int:student_id>/update' , methods=["GET" ,"POST"])
 def update_student(student_id):
     if request.method == "GET":
-        student = Student.query.filter_by(student_id = student_id)
+        student = Student.query.filter_by(student_id = student_id).first()
         return render_template('update_student.html' , student=student)
     elif request.method == "POST":
-        student = Student.query.filter_by(student_id = student_id)
+        student = Student.query.filter_by(student_id = student_id).first()
         student.roll_number = request.form['roll']
         student.first_name = request.form['f_name']
         student.last_name = request.form['l_name']
@@ -78,9 +78,10 @@ def update_student(student_id):
         Enrollments.query.filter_by(estudent_id = student_id).delete()
         
         for course in courses:
-            course_details = Course.query.filter_by(course_name = course)
+            course_details = Course.query.filter_by(course_code = course).first()
             enrollment = Enrollments(estudent_id = student_id , ecourse_id = course_details.course_id)
             db.session.add(enrollment)
+        
         db.session.commit()
         
         return redirect(url_for('home'))
@@ -88,12 +89,12 @@ def update_student(student_id):
         return render_template('error.html')
     
     
-@app.route('/student/<int:student_id>/delete' , method=['GET'])
+@app.route('/student/<int:student_id>/delete' , methods=['GET'])
 def delete_student(student_id):
     if request.method == "GET":
         student = Student.query.filter_by(student_id = student_id).first()
         db.session.delete(student)
-        db.session.commt()
+        db.session.commit()
         Enrollments.query.filter_by(estudent_id = student_id).delete()
         db.session.commit()
         return redirect(url_for('home'))
@@ -101,17 +102,19 @@ def delete_student(student_id):
         return render_template('error.html')
     
     
-@app.route('/student/<int:student_id' , methods=['GET'])
+@app.route('/student/<int:student_id>' , methods=['GET'])
 def view_student(student_id):
     if request.method == "GET":
         student = Student.query.filter_by(student_id = student_id).first()
-        enrollments = Enrollments.query.filter_by(student_id = student_id).all()
+        enrollments = Enrollments.query.filter_by(estudent_id = student_id).all()
         courses = []
         for enrollment in enrollments:
-            course = Course.query.filter_by(course_id = enrollment.course_id).first()
+            course = Course.query.filter_by(course_id = enrollment.ecourse_id).first()
             courses.append(course)
         
         return render_template('view_student.html' , student=student , courses = courses)
+    else:
+        return render_template('error.html')
         
         
 
